b5e140dee708571e4c7e70cb9ad9c38d7209afda
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-10-09 16:05:08 +0100

    add stuff

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_URI, { dialect: "mysql" });
+const sequelize = new Sequelize(process.env.DB_URI, {
+  dialect: "mysql",
+  dialectModule: require("mysql2"),
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 sequelize.authenticate();
 
