1b8531b63ee8780e2226f571c21f3450b35360c0
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-10-09 16:39:43 +0100

    add dialect module

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,12 @@
 const { Sequelize } = require("sequelize");
 
+const database = process.env.DATABASE || "";
+const username = process.env.USERNAME || "";
+const password = process.env.PASSWORD || "";
+
 const sequelize = new Sequelize(process.env.DB_URI, {
   dialect: "mysql",
+  dialectModule: require("mysql2"),
 });
 
 sequelize.authenticate();
