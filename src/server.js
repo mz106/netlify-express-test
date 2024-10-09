3e8f40ade98c8411d1fcd427a5c0d57e48aef0f6
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-10-09 13:59:15 +0100

    add code

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const { Router } = require("express");
+
+const netlifyRouter = Router();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "App is runnning" });
+});
+
+app.use("/.netlify/functions/server", netlifyRouter);
+
+module.exports = app;
+module.exports.handler = serverless(app);
